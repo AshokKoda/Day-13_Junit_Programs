e80641792bade6ca66cef4ffb9cb1d5d8f9b580c
Ashok Koda <a.koda59@gmail.com>
2021-12-19 14:06:53 +0530

    Max number at 1st position

--- src/main/java/com/bridgelabz/junittests/FindmaxNumber.java
+++ src/main/java/com/bridgelabz/junittests/FindmaxNumber.java
@@ -1,25 +1,9 @@
 package com.bridgelabz.junittests;
 
-import java.util.Scanner;
-
 public class FindmaxNumber {
-
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		Integer a, b, c;
-		System.out.println("Enter A value: ");
-		a = sc.nextInt();
-		System.out.println("Enter B value: ");
-		b = sc.nextInt();
-		System.out.println("Enter C value: ");
-		c = sc.nextInt();
-		
-		System.out.println(a.compareTo(b) + " is the maximum number.");
-		System.out.println(b.compareTo(a) + " is the maximum number.");
-		System.out.println(b.compareTo(c) + " is the maximum number.");
-		System.out.println(c + " is the maximum number.");
-
+	
+	public int maxNumAtPositionOne(int a, int b, int c) {
+		return c > (a > b ? a : b) ? c : ((a > b) ? a : b);
 	}
 
 }
