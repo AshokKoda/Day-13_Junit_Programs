826eb7cfcd26cc1a93de7f9ba750d7279c1653b9
Ashok Koda <a.koda59@gmail.com>
2021-12-19 14:10:39 +0530

    Max number at 2nd position

--- src/test/java/com/bridgelabz/junit/FindMaxNumTestcase.java
+++ src/test/java/com/bridgelabz/junit/FindMaxNumTestcase.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 
 public class FindMaxNumTestcase {
 
+	/*
+	 * Max number at 1st position
+	 */
 	@Test
 	public void testPass() {
 		int a = 15, b = 10, c = 5;
@@ -16,6 +19,23 @@ public class FindMaxNumTestcase {
 	@Test
 	public void testFail() {
 		int a = 15, b = 10, c = 5;
+		int result = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+		Assert.assertEquals(10, result);
+	}
+	
+	/*
+	 * Max number at 2nd position
+	 */
+	@Test
+	public void testPass2() {
+		int a = 15, b = 20, c = 5;
+		int result = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
+		Assert.assertEquals(20, result);
+	}
+	
+	@Test
+	public void testFail2() {
+		int a = 15, b = 20, c = 5;
 		int result = c > (a > b ? a : b) ? c : ((a > b) ? a : b);
 		Assert.assertEquals(10, result);
 	}
